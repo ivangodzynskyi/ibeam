38252225f0bc7b89e0fbaf875c346da5eedf781e
Ivan Godzynskyi <ivan.godzynskyi@gmail.com>
2026-03-12 23:48:39 +0200

    Rename Config

--- config.py
+++ config.py
@@ -6,9 +6,11 @@ from dataclasses import dataclass
 
 
 @dataclass
-class MeanderConfig:
+class Config:
     radius: float = 0.2
     length: float = 6.0
-    height: float = 2.0
+    height: float = 0.1
     periods: int = 6
-    k_angle: float = 0.0
+    k_angle: float = 0.1
+    h1: float = 0.2
+    h2: float = 0.3
