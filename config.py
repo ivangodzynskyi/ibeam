39cd4a146f5511ffe74d47149ab5ccd7491ebe88
Ivan Godzynskyi <ivan.godzynskyi@gmail.com>
2026-06-09 13:42:47 +0300

    Make height near the experiment

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     radius: float = 0.01
-    length: float = 3.0
-    height: float = 0.1
-    periods: int = 12
-    k_angle: float = 0.0
-    h1: float = 0.5
-    h2: float = 0.2
-    bf: float = 0.2       # ширина полки (flange width, Y-direction)
-    nb: int = 8           # кількість проміжків на кожній стороні полки
-    tw: float = 0.008     # товщина стінки
-    tb: float = 0.012     # товщина полки
+    length: float = 1.0         #довжина півбалки
+    height: float = 0.07
+    periods: int = 8
+    k_angle: float = 0.02
+    h1: float = 0.182
+    h2: float = 0.112       
+    bf: float = 0.12       # ширина полки (flange width, Y-direction)
+    nb: int = 3           # кількість проміжків на кожній стороні полки
+    tw: float = 0.0062     # товщина стінки
+    tb: float = 0.0098     # товщина полки
